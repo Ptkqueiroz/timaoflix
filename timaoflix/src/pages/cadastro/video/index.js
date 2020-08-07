549d3e57c5a18d317fda908849040213460c365d
ptkqueiroz <patrick.queiroz@icloud.com>
2020-08-07 20:52:40 -0300

    formulario cadastro

--- timaoflix/src/pages/cadastro/video/index.js
+++ timaoflix/src/pages/cadastro/video/index.js
@@ -2,17 +2,32 @@ import React from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
-function CadastroVideo (){
-    return (
-      <PageDefault>
-        <h1>Cadastro de Video</h1>
+function CadastroVideo() {
+  return (
+    <PageDefault>
+      <h1>Cadastro de Video</h1>
 
-        <Link to="/cadastro/categoria">
-          Cadastrar Categoria
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+
+      <Link to="/cadastro/categoria">
+        Cadastrar Categoria
         </Link>
-      </PageDefault>
-    )
-  }
+    </PageDefault>
+  )
+}
 
 
-  export default CadastroVideo;
+export default CadastroVideo;
--- timaoflix/src/pages/categoria/index.js
+++ timaoflix/src/pages/categoria/index.js
@@ -1,11 +1,89 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PageDefault from '../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import FormField from '../../components/FormField';
 
 function CadastroCategoria(){
+  const [categorias, setCategorias] = useState([])
+
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  }
+
+  const [values,setValues] = useState(valoresIniciais);
+  
+  function setValue (chave, valor){
+    setValues({
+      // chave: nome, descrição
+      ...values,
+      [chave]: valor, //nome: 'valor'
+    })
+  }
+
+  function handleChange(infosDoEvento){
+    setValue(
+    infosDoEvento.target.getAttribute('name'), 
+    infosDoEvento.target.value);
+  }
+
     return (
       <PageDefault>
-        <h1>Cadastro de Categoria</h1>
+        <h1>Cadastro de Categoria: {values.nome}</h1>
+
+        <form onSubmit={function handleSubmit(infosDoEvento){
+          infosDoEvento.preventDefault();          
+          setCategorias([
+            ...categorias,
+            values
+          ]);
+
+          setValues(valoresIniciais)
+
+        }}>
+
+          <FormField
+          label = "Nome da Categoria"
+          type= "text"
+          name="nome"
+          value={values.nome}
+          onChange = {handleChange}
+          />
+
+          <FormField
+          label = "Descrição"
+          type= "textarea"
+          name="descricao"
+          value={values.descricao}
+          onChange = {handleChange}
+          />
+
+          <FormField
+          label = "Cor"
+          type= "color"
+          name="cor"
+          value={values.cor}
+          onChange = {handleChange}
+          />
+
+
+<button>
+  Cadastrar
+</button>
+</form>
+
+<ul>
+{categorias.map((categoria, indice)=>{
+  return (
+  <li key={`${categoria}${indice}`}>
+    {categoria.nome}
+  </li>
+  )
+})}
+
+</ul>
+
 
         <Link to="/">
           Ir Para Home
